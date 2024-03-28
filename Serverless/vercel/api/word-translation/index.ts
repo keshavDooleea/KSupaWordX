daa3d3c6ed9f94f84941abd1707c8525a2d0089a
keshav <kdooleea@yahoo.ca>
2024-03-27 23:40:11 -0400

    feat: added vercel logs

--- Serverless/vercel/api/word-translation/index.ts
+++ Serverless/vercel/api/word-translation/index.ts
@@ -1,7 +1,11 @@
 import { NowRequest, NowResponse } from "@now/node";
 
 const handler = async (req: NowRequest, res: NowResponse) => {
-  console.log({ req });
+  const { id: wordId, lang, word } = await req.body;
+
+  console.log("GOT IT");
+  console.log({ wordId, lang, word });
+
   res.send({ message: "Vercel typescript API boilerplate kkk" });
 };
 
